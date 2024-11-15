dbe22eb5be032d9f4cc5751fd544201a14532c29
keerthybreddy <keerthybreddy@gmail.com>
2024-11-15 13:34:26 -0800

    working registration page that sends data to a mysql database

--- tune-share/server/server.js
+++ tune-share/server/server.js
@@ -1,8 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["user1", "user2", "user3", "user4"]})
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const db = mysql.createPool({
+    connectionLimit: 10,
+    host : 'localhost',
+    user : 'root',
+    password : 'password', //place your own mysql account password
+    database : 'TuneShareDB'
+});
+
+app.post('/registration-page', (req, res) => {
+    const username = req.body.username;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const password = req.body.password;
+    const email = req.body.email;
+    db.query("INSERT INTO User (username, firstName, lastName, password, email) VALUES (?, ?, ?, ?, ?)", [username, firstName, lastName, password, email], (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send({username: username})
+        }
+    })
 })
 
 app.listen(5000, () => {console.log("Server started on port 5000")})
