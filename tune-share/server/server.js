9522d25febc9caa1e0a863b9b86b415917aac05f
jcamachoSJSU <jadon.camacho@sjsu.edu>
2024-11-20 00:03:09 -0800

    took out my password

--- tune-share/server/server.js
+++ tune-share/server/server.js
@@ -12,7 +12,7 @@ const db = mysql.createPool({
     connectionLimit: 10,
     host : 'localhost',
     user : 'root',
-    password : 'zorbak123', //place your own mysql account password
+    password : 'password', //place your own mysql account password
     database : 'TuneShareDB'
 });
 
